232d47601f268aac7be511fc998d037f75d9ca01
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-01-02 09:39:22 +0100

    refactor: extract into variable

--- server.js
+++ server.js
@@ -5,27 +5,21 @@ const next = require('next')
 
 const routes = require('./routes')
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
+const { NODE_ENV = 'development', PORT = '3000', DEPLOY_DATE } = process.env
+
+const deployDate = DEPLOY_DATE
+  ? new Date(parseInt(process.env.DEPLOY_DATE, 10) * 1000).toISOString()
+  : 'n/a'
+
+const port = parseInt(PORT, 10)
+const dev = NODE_ENV !== 'production'
 
 const app = next({ dev })
 const handler = routes.getRequestHandler(app)
 
 app.prepare().then(() => {
   const server = express()
-
-  server.get('/api/status', (req, res) => {
-    let deployDate = 'n/a';
-    if (process.env.DEPLOY_DATE) {
-      const date = new Date(parseInt(process.env.DEPLOY_DATE, 10) * 1000)
-      deployDate = date.toISOString()
-    }
-    res.json({ deployDate })
-  })
-
-  server.get('*', (req, res) => {
-    handler(req, res)
-  })
-
+  server.get('/api/status', (req, res) => res.json({ deployDate }))
+  server.get('*', (req, res) => handler(req, res))
   server.listen(port, () => console.log(`> Ready on http://localhost:${port}`))
 })
