82b6d09f8b316f4a631537a5e861d5afae1a3b67
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-01-04 13:22:53 +0100

    build: remove unnecessary components

--- core/get-readme.js
+++ core/get-readme.js
@@ -6,16 +6,36 @@ import cheerio from 'cheerio'
 import url from 'url'
 import remark from 'remark'
 import remarkHtml from 'remark-html'
+import remarkPreset from 'remark-preset-lint-recommended'
+
 import memoize from './memoize'
 
 const { GITHUB_TOKEN } = process.env || {}
 
-const remarkProcessor = remark().use(remarkHtml)
+const remarkProcessor = remark()
+  .use(remarkPreset)
+  .use(remarkHtml)
 
 const md2html = async md => {
   const { contents } = await remarkProcessor.process(md)
   return contents
 }
+
+// const md2html = async (text, { owner, repo }) => {
+//   const res = await fetch(`https://api.github.com/markdown`, {
+//     method: 'post',
+//     body: JSON.stringify({
+//       text,
+//       mode: 'gfm',
+//       context: `${owner}/${repo}`
+//     }),
+//     headers: {
+//       'Content-Type': 'text/plain'
+//     }
+//   })
+
+//   return res.text()
+// }
 
 const loadHTML = html =>
   cheerio.load(html, {
@@ -45,7 +65,7 @@ export default memoize(async ({ owner, repo }) => {
   )
 
   const markdown = await res.text()
-  const html = await md2html(markdown)
+  const html = await md2html(markdown, { owner, repo })
   const $ = loadHTML(html)
 
   forEach(TAGS, (htmlTags, propName) => {
