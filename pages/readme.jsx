2713b1562a083dba0b241b95d3ecc1e66d0ea550
Axel Hernández Ferrera <axelhzf@gmail.com>
2019-01-04 12:09:54 +0000

    build: use react hooks

--- pages/readme.jsx
+++ pages/readme.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, Component } from 'react'
+import React, { Fragment } from 'react'
 import { Container } from 'components'
 import { getReadme, getMeta, buildMeta } from 'core'
 import ScrollProgress from 'scrollprogress'
@@ -7,48 +7,42 @@ import NProgress from 'nprogress'
 
 import Head from 'next/head'
 
-export default class Readme extends Component {
-  constructor (props) {
-    super(props)
+export default function Readme(props) {
+  useProgressBar()
+  const { meta, readme } = props
+  return (
+    <Fragment>
+      <Head>{buildMeta(meta)}</Head>
+      <Container>
+        <Markdown escapeHtml={false} source={readme} />
+      </Container>
+    </Fragment>
+  )
+}
 
-    if (global.document) {
-      NProgress.configure({
-        trickle: false,
-        trickleSpeed: 0,
-        speed: 0,
-        showSpinner: false
-      }).start()
-    }
+Readme.getInitialProps = async ({ query}) => {
+  if (query) {
+    const [readme, meta] = await Promise.all([
+      getReadme(query),
+      getMeta(query)
+    ])
+    return { meta, readme }
   }
-  static async getInitialProps ({ query }) {
-    if (query) {
-      const [readme, meta] = await Promise.all([
-        getReadme(query),
-        getMeta(query)
-      ])
-      return { meta, readme }
-    }
-  }
+}
 
-  componentDidMount () {
-    this.progressObserver = new ScrollProgress((x, y) => {
+function useProgressBar() {
+  React.useEffect(() => {
+    NProgress.configure({
+      trickle: false,
+      trickleSpeed: 0,
+      speed: 0,
+      showSpinner: false
+    }).start()
+
+    const progressObserver = new ScrollProgress((x, y) => {
       y >= 0.999 ? NProgress.set(0.999) : NProgress.set(y)
     })
-  }
 
-  componentWillUnmount () {
-    this.progressObserver.destroy()
-  }
-
-  render () {
-    const { meta, readme } = this.props
-    return (
-      <Fragment>
-        <Head>{buildMeta(meta)}</Head>
-        <Container>
-          <Markdown escapeHtml={false} source={readme} />
-        </Container>
-      </Fragment>
-    )
-  }
+    return () => progressObserver.destroy()
+  }, [])
 }
