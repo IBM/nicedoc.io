7b65127a6b5b0a220f9936c5df3853fdbaf1f2b1
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-02-08 15:24:21 +0100

    build: pass variable from travis

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const pkg = require('./package.json')
 
 const url = require('url')
 
-const { SITE_URL } = process.env
+const { SITE_URL = pkg.homepage } = process.env
 
 const config = {
   'process.env.APP.name': pkg.name,
@@ -11,8 +11,14 @@ const config = {
   'process.env.APP.image': url.resolve(SITE_URL, '/static/banner.jpg'),
   'process.env.APP.logo': url.resolve(SITE_URL, '/static/logo.png'),
   'process.env.APP.favicon.ico': url.resolve(SITE_URL, '/static/favicon.ico'),
-  'process.env.APP.favicon.medium': url.resolve(SITE_URL, '/static/favicon-32x32.png'),
-  'process.env.APP.favicon.small': url.resolve(SITE_URL, '/static/favicon-16x16.png')
+  'process.env.APP.favicon.medium': url.resolve(
+    SITE_URL,
+    '/static/favicon-32x32.png'
+  ),
+  'process.env.APP.favicon.small': url.resolve(
+    SITE_URL,
+    '/static/favicon-16x16.png'
+  )
 }
 
 module.exports = config
