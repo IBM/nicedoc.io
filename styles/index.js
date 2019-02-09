ebffccdef1c81d04b087859b18bda25b94b17436
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-02-09 17:34:14 +0100

    feat: add dark mode
    closes #2

--- styles/index.js
+++ styles/index.js
@@ -3,10 +3,6 @@ export const navbar = 65
 
 export const breakpoints = ['40em', '52em', '64em']
 export const fontSizes = [12, 14, 16, 20, 24, 32, 48, 64]
-export const colors = {
-  blue: '#07c',
-  lightgray: '#f6f6ff'
-}
 export const space = [0, 4, 8, 16, 32, 64, 128, 256]
 export const fonts = {
   sans: 'system-ui, sans-serif',
@@ -15,6 +11,11 @@ export const fonts = {
 export const shadows = {
   small: '0 0 4px rgba(0, 0, 0, .125)',
   large: '0 0 24px rgba(0, 0, 0, .125)'
+}
+
+export const colors = {
+  black: '#000',
+  white: '#fff'
 }
 
 export default {
