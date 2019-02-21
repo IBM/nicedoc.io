a6ce320e12cd0a6ec49d1789c052212900d9edbf
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-02-21 23:48:03 +0100

    fix: pass props

--- components/icons/forks.js
+++ components/icons/forks.js
@@ -1,6 +1,6 @@
 import Svg from '../svg'
 
-export default ({ style, ...props }) => (
+export default props => (
   <Svg viewBox='0 0 10 16' xmlns='http://www.w3.org/2000/svg' {...props}>
     <path
       fillRule='evenodd'
--- components/icons/ruby.js
+++ components/icons/ruby.js
@@ -1,6 +1,6 @@
 import Svg from '../svg'
 
-export default ({ ...props }) => (
+export default props => (
   <Svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' {...props}>
     <path
       fillRule='evenodd'
