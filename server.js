30012a1d06c4b186fa0d0c2b26e4bc7428ba4783
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-02-22 12:27:16 +0100

    build: use cacheable response module

--- server.js
+++ server.js
@@ -1,30 +1,22 @@
 'use strict'
 
-const Keyv = require('keyv')
-
-const { resolve: urlResolve } = require('url')
-const normalizeUrl = require('normalize-url')
-const { reduce } = require('lodash')
-const prettyMs = require('pretty-ms')
+const cacheableResponse = require('cacheable-response')
 const express = require('express')
-const { URL } = require('url')
 const next = require('next')
 
 const routes = require('./routes')
-const pkg = require('./package.json')
 
-const {
-  SITE_URL = pkg.homepage,
-  NODE_ENV = 'development',
-  PORT = '3000',
-  DEPLOY_DATE
-} = process.env
+const { NODE_ENV = 'development', PORT = '3000', DEPLOY_DATE } = process.env
 
-const INTERNAL_NEXT_PATHS = ['/_next', '/_webpack/', '/__webpack_hmr', '/static/']
-const TTL = 1000 * 60 * 60 * 2 // 2 hours
-const CACHE_NAMESPACE = 'ssr'
+const INTERNAL_NEXT_PATHS = [
+  '/_next',
+  '/_webpack/',
+  '/__webpack_hmr',
+  '/static/'
+]
 
-const isNextPath = ({ url }) => INTERNAL_NEXT_PATHS.some(path => url.startsWith(path))
+const isNextPath = ({ url }) =>
+  INTERNAL_NEXT_PATHS.some(path => url.startsWith(path))
 
 const isProduction = NODE_ENV === 'production'
 const isStaging = NODE_ENV === 'staging'
@@ -36,80 +28,26 @@ const deployDate = DEPLOY_DATE
 const port = parseInt(PORT, 10)
 const dev = !isProduction
 
-const getCacheKey = url => {
-  const { origin } = new URL(url)
-  const baseKey = normalizeUrl(url, {
-    removeQueryParameters: ['force', 'filter', 'ttl', 'utm', 'ref']
-  })
-  return baseKey.replace(origin, '').replace('/?', '')
-}
-
-const getQueryParams = query =>
-  reduce(query, (acc, value, key) => ({ ...acc, [key]: value === '' ? true : value }), {})
-
-const setCacheControl = ({ res, createdAt, isHit, ttl, hasForce }) => {
-  // Specifies the maximum amount of time a resource
-  // will be considered fresh in seconds
-  const diff = hasForce ? 0 : createdAt + ttl - Date.now()
-  const maxAge = Math.floor(diff / 1000)
-  res.setHeader(
-    'Cache-Control',
-    `public, max-age=${maxAge}, s-maxage=${maxAge}, stale-while-revalidate=30`
-  )
-  res.setHeader('X-Cache-Status', isHit ? 'HIT' : 'MISS')
-  res.setHeader('X-Cache-Expired-At', prettyMs(diff))
-}
-
-const renderAndCache = ssrCache => async (req, res) => {
-  if (isNextPath(req)) return handle(req, res)
-
-  const url = urlResolve(SITE_URL, req.url)
-  const key = getCacheKey(url)
-
-  const { route, params, parsedUrl } = routes.match(key)
-  if (!route) return handle(req, res, parsedUrl)
-
-  const { force: hasForce } = getQueryParams(req.query)
-  const { html: cachedHtml, createdAt } = (await ssrCache.get(key)) || {}
-  const isHit = !hasForce && !!cachedHtml
-
-  // If we have a page in the cache, let's serve it
-  if (isHit) {
-    setCacheControl({ res, createdAt, isHit, hasForce, ttl: TTL })
-    return res.send(cachedHtml)
-  }
-
-  try {
-    // If not let's render the page into HTML
-    const html = await app.renderToHTML(req, res, route.page, params)
-
-    // Something is wrong with the request, let's skip the cache
-    if (res.statusCode !== 200) {
-      res.send(html)
-      return
-    }
-
-    const createdAt = Date.now()
-    await ssrCache.set(key, { html, createdAt }, TTL)
-    setCacheControl({ res, createdAt, isHit, hasForce, ttl: TTL })
-    res.send(html)
-  } catch (err) {
-    app.renderError(err, req, res, route.page, params)
-  }
-}
-
 const app = next({ dev })
 
 const handle = routes.getRequestHandler(app)
 
 const middleware = (() => {
   if (!isProduction && !isStaging) return (req, res) => handle(req, res)
-  return renderAndCache(
-    new Keyv({
-      namespace: CACHE_NAMESPACE,
-      ttl: TTL
-    })
-  )
+
+  const ssrCache = cacheableResponse({
+    get: async ({ req, res, route, params }) => ({
+      data: await app.renderToHTML(req, res, route.page, params)
+    }),
+    send: ({ res, data }) => res.send(data)
+  })
+
+  return (req, res) => {
+    if (isNextPath(req)) return handle(req, res)
+    const { route, params, parsedUrl } = routes.match(req.url)
+    if (!route) return handle(req, res, parsedUrl)
+    return ssrCache({ req, res, route, params })
+  }
 })()
 
 app.prepare().then(() => {
