c42d6b83fe0406c068d178a604f9bf65c9fc3440
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-04-03 17:41:47 +0200

    build: remove cache

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 'use strict'
 
-const cacheableResponse = require('cacheable-response')
 const express = require('express')
 const next = require('next')
 
@@ -8,18 +7,7 @@ const routes = require('./routes')
 
 const { NODE_ENV = 'development', PORT = '3000', DEPLOY_DATE } = process.env
 
-const INTERNAL_NEXT_PATHS = [
-  '/_next',
-  '/_webpack/',
-  '/__webpack_hmr',
-  '/static/'
-]
-
-const isNextPath = ({ url }) =>
-  INTERNAL_NEXT_PATHS.some(path => url.startsWith(path))
-
 const isProduction = NODE_ENV === 'production'
-const isStaging = NODE_ENV === 'staging'
 
 const deployDate = DEPLOY_DATE
   ? new Date(parseInt(process.env.DEPLOY_DATE, 10) * 1000).toISOString()
@@ -33,21 +21,7 @@ const app = next({ dev })
 const handle = routes.getRequestHandler(app)
 
 const middleware = (() => {
-  if (!isProduction && !isStaging) return (req, res) => handle(req, res)
-
-  const ssrCache = cacheableResponse({
-    get: async ({ req, res, route, params }) => ({
-      data: await app.renderToHTML(req, res, route.page, params)
-    }),
-    send: ({ res, data }) => res.send(data)
-  })
-
-  return (req, res) => {
-    if (isNextPath(req)) return handle(req, res)
-    const { route, params, parsedUrl } = routes.match(req.url)
-    if (!route) return handle(req, res, parsedUrl)
-    return ssrCache({ req, res, route, params })
-  }
+  return (req, res) => handle(req, res)
 })()
 
 app.prepare().then(() => {
