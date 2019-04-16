22d094efa0a2025b6ecdc786b41b10149ff63926
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-04-16 14:58:53 +0200

    refactor: better landing page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,32 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect, Fragment, Component } from 'react'
 import { Li, Text, Container, Box, Flex, Link } from 'components'
+
+const ThemeToggle = () => {
+  const [theme, setTheme] = useState(null)
+  useEffect(() => {
+    setTheme(window.__theme)
+    window.__onThemeChange = () => setTheme(window.__theme)
+  }, [])
+
+  const isDark = theme === 'dark'
+  const isLight = !isDark
+
+  return (
+    <Fragment>
+      <span
+        className={isDark ? 'blue' : undefined}
+        style={{ cursor: isDark ? 'pointer' : 'inherit' }}
+        onClick={() => window.__setPreferredTheme('light')}
+      >Light</span>
+      <span> / </span>
+      <span
+        className={isLight ? 'blue' : undefined}
+        onClick={() => window.__setPreferredTheme('dark')}
+        style={{ cursor: isLight ? 'pointer' : 'inherit' }}
+      >Dark</span>
+    </Fragment>
+  )
+}
 
 export default class Index extends Component {
   render () {
@@ -17,7 +44,7 @@ export default class Index extends Component {
 
           <Box style={{ textAlign: 'center' }}>
             <Text style={{ margin: 'auto' }} maxWidth={'24em'} pt={3}>
-              pretty README as service.
+              pretty README as service (<Text fontSize='16px' as='span'><Link icon={false} href='https://github.com/IBM/nicedoc.io'>source code</Link></Text>).
             </Text>
             <Box py={3}>
               <code>nicedoc.io/:org/:repo[@:ref]</code>
@@ -26,24 +53,27 @@ export default class Index extends Component {
           <Box>
             <h2>Features</h2>
             <ul>
-              <Li>
-                Automatically generated documentation sites for your markdown
-                files.
-              </Li>
-              <Li>Compatible with any doc file hosted on github.com.</Li>
-              <Li>
-                Point to a git hash reference in any{' '}
-                <Link href='https://github.com/github/markup#markups'>
-                  markup
-                </Link>{' '}
-                format.
-              </Li>
-              <Li>Light & Dark mode support.</Li>
+              <Li>Instant beauty documentation for github.com.</Li>
+              <Li>Compatible with any <Link href='https://github.com/github/markup#markups'>markup</Link>{' '} format.</Li>
+              <Li>Point to any git hash or branch.</Li>
+              <Li>File-system based routing.</Li>
+              <Li><ThemeToggle /> theme support.</Li>
             </ul>
             <h2>Why</h2>
             <Text>
-              Software documentation is a lie: nobody wants to do it but
-              everyone wants to have it.
+              <b>nicedoc.io</b> aims to reduce the documentation friction, converting any markup file hosted on github.com into an instant beauty documentation.
+            </Text>
+            <Text fontSize={2}>
+              <Text mb={0} fontSize={1}>any repository</Text>
+              <Link href='/sindresorhus/got'>nicedoc.io/sindresorhus/got</Link>
+            </Text>
+            <Text fontSize={2}>
+              <Text mb={0} fontSize={1}>any markup file into subdirectory</Text>
+              <Link href='/thejameskyle/babel-handbook/translations/en/user-handbook.md'>nicedoc.io/thejameskyle/…/user-handbook.md</Link>
+            </Text>
+            <Text fontSize={2}>
+              <Text mb={0} fontSize={1}>any git reference</Text>
+              <Link href='/GoogleChromeLabs/quicklink@1.0.0'>nicedoc.io/GoogleChromeLabs/quicklink@1.0.0</Link>
             </Text>
             <Text>
               We can't write your project documentation for you, but we can help
@@ -51,9 +81,6 @@ export default class Index extends Component {
             </Text>
             <h2>Examples</h2>
             <ul>
-              <Li>
-                <Link href='/sindresorhus/got'>sindresorhus/got</Link>
-              </Li>
               <Li>
                 <Link href='/substack/stream-handbook'>
                   substack/stream-handbook
@@ -68,11 +95,6 @@ export default class Index extends Component {
               <Li>
                 <Link href='/jamiebuilds/babel-handbook'>
                   jamiebuilds/babel-handbook
-                </Link>
-              </Li>
-              <Li>
-                <Link href='/GoogleChromeLabs/quicklink'>
-                  GoogleChromeLabs/quicklink
                 </Link>
               </Li>
               <Li>
