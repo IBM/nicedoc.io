c0753ef55d2bed7c24238f038bc80980da935b09
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-04-17 12:43:59 +0200

    docs: add first run tips

--- README.md
+++ README.md
@@ -4,10 +4,33 @@
 
 ## Development
 
-After `npm install`:
+### Environment Variables
 
-- **development**: `npm run dev`.
-- **production**: `npm run build && npm start`.
+#### GITHUB_TOKEN
+
+*Required*
+
+Since we are interacting with GitHub API, you need to setup a token for authenticating the requests.
+
+![](https://i.imgur.com/6cmrVud.png)
+
+See more at [Creating a personal access token for the command line](https://help.github.com/en/articles/creating-a-personal-access-token-for-the-command-line) on GitHub Help.
+
+#### PORT
+
+default: `3000`</br>
+
+The port to be used for running the HTTP server.
+
+#### SITE_URL
+
+default: `http://localhost:3000`</br>
+
+It sets the base URL to be used for resolving relative URLs detected on GitHub.
+
+### Getting Started
+
+After `npm install`, just do a `npm run dev`.
 
 ## License
 
--- constants.js
+++ constants.js
@@ -1,16 +1,39 @@
 'use strict'
 
-const pkg = require('./package.json')
+const terminalLink = require('terminal-link')
+const chalk = require('chalk')
 const url = require('url')
 
-const { SITE_URL = pkg.homepage, DEPLOY_DATE, PORT = 3000, NODE_ENV = 'development' } =
-  process.env || {}
+const pkg = require('./package.json')
+
+const {
+  SITE_URL = pkg.homepage,
+  DEPLOY_DATE,
+  PORT = 3000,
+  NODE_ENV = 'development',
+  GITHUB_TOKEN
+} = process.env || {}
+
+const isDevelopment = NODE_ENV === 'development'
+
+if (!GITHUB_TOKEN) {
+  const helpUrl = 'https://github.com/IBM/nicedoc.io'
+
+  const helpLink = terminalLink('See how to', helpUrl, {
+    fallback: () => `See how to at ${helpUrl}`
+  })
+
+  const message = `${chalk.red('Error:')} You need to setup \`GITHUB_TOKEN\`. ${helpLink}.\n`
+
+  console.error(message)
+  process.exit(1)
+}
 
 module.exports = {
   REGEX_LOCAL_URL: /^\/(?!\/)/,
   REGEX_HTTP_PROTOCOL: /^https?:\/\//i,
   REGEX_START_WITH_LETTER_OR_NUMBER: /^[a-z0-9]/i,
-  SITE_URL,
+  SITE_URL: isDevelopment ? 'http://localhost:3000' : SITE_URL,
   DEPLOY_DATE: DEPLOY_DATE ? new Date(parseInt(DEPLOY_DATE), 10 * 1000) : 'n/a',
   PORT: parseInt(PORT, 10),
   NODE_ENV,
