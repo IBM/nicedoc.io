42c91486f4f1743fb035fa12faf6bd308a3b88c5
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-06-27 09:15:04 +0200

    fix: handle absolute urls
    closes #85

--- core/github/build-readme/util.js
+++ core/github/build-readme/util.js
@@ -1,11 +1,27 @@
 import fileExtension from 'file-extension'
-import url from 'url'
+import isRelativeUrl from 'is-relative-url'
+import url, { URL } from 'url'
+
+export const isGithubUrl = url => {
+  try {
+    return new URL(url).hostname === 'github.com'
+  } catch (err) {
+    return false
+  }
+}
 
 export const resolveAssetUrl = (src, { path, owner, repo, ref }) => {
-  if (!path.startsWith('/')) path = `/${path}`
   const slug = `${owner}/${repo}`
-  const base = `https://raw.githubusercontent.com/${slug}/${ref}${path}`
-  return url.resolve(base, src)
+
+  if (isRelativeUrl(src)) {
+    const base = `https://raw.githubusercontent.com/${slug}/${ref}/${path}`
+    if (src.startsWith('/')) src = src.substring(1)
+    return url.resolve(base, src)
+  }
+
+  if (!isGithubUrl(src)) return src
+
+  return src.replace('github.com', 'raw.githubusercontent.com').replace(`blob/`, '')
 }
 
 export const resolveUrl = (from, to) => {
